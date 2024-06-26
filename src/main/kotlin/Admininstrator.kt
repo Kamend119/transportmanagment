603882c51ea57a6f53fc313e57c27e4658b4303d
kamend119 <a.mikryukova119@mail.ru>
2024-06-26 11:35:37 +0500

    ._.

--- src/main/kotlin/Admininstrator.kt
+++ src/main/kotlin/Admininstrator.kt
@@ -1,3 +1,4 @@
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -5,6 +6,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -194,7 +196,13 @@ fun DriverPerformance(onLogout: (Pages) -> Unit) {
                 LazyColumn {
                     items(data.size) { index ->
                         val trip = data[index]
-                        Row {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color.Transparent)
+                                .padding(8.dp),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
                             trip.forEach { item ->
                                 TableCell(text = item)
                             }
